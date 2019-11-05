c1ead1ca7f5af7f6f881d5dc2db6e559244c88a5
ad-aeternum <57330780+ad-aeternum@users.noreply.github.com>
2019-11-05 19:26:39 +0300

    Revert "footer created"
    This reverts commit 1b9a9e3671f9dfe2aacae4f7e0be0708c503c385.

--- script.js
+++ script.js
@@ -6,4 +6,10 @@ function close() {
 	document.getElementById('reg').style.display="none";
 }
 
+function reg() {
+	let a,b;
+	document.getElementById('login') = a;
+	document.getElementById('password') = b;
+	alert(a,b);
 
+}
